4bd2cc02f3d701846d8dcf82c6e5714956158ac7
Mhnd-DS <mohannad.sabbahi@gmail.com>
2022-05-01 14:30:32 +0300

    printf file

---
+++ printf.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May  1 01:00:04 2022
+
+@author: themassiveboy
+"""
+
+print('(%s) - (%s) - (%s) ' %  ('PJXIO' , 'Fury' , 'Sa'))
+
+
+print('({0}) - ({0}) - ({0}) '.format('PJXIO' , 'Fury' , 'Sa'))
+
+    
